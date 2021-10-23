80e5fa74b03227cddf513c6d1cd36ea56c769ebf
Adrian <hannsadrian@gmail.com>
2021-10-23 13:13:06 +0200

    Add first version of chess piece

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
 import Field from './chessfield/field'
-
+import whitePiece from './assets/img/white/piece.png'
 
 function App() {
   const fieldRef = useRef(null)
@@ -19,11 +19,11 @@ function App() {
   return (
     <div className="w-screen h-screen bg-teal-900 bg-clouds">
       <div className='flex items-center justify-center h-screen'>
-      <div style={{transform: "translate(-"+x/2+"px, -"+y/2+"px)"}}>
-        <Field ref={fieldRef}/>
+        <img className='absolute z-50 w-8 -translate-y-7 -translate-x-14' src={whitePiece}/>
+        <div style={{transform: "translate(-"+x/2+"px, -"+y/2+"px)"}}>
+          <Field ref={fieldRef}/>
+        </div>
       </div>
-      </div>
-      <p>{x} {y}</p>
     </div>
   )
 }
